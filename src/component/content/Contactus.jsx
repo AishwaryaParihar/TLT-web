635fbada0faaca495f2f5bb7e5a0670f83966086
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-07-03 16:53:52 +0530

    ss

--- src/component/content/Contactus.jsx
+++ src/component/content/Contactus.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import contactimg from '../../assets/contact-img.webp'
-import gif from '../../assets/git/air.gif'
+
 
 const Contactus = () => {
   return (
     <>
     <img src={contactimg} alt="" />
-    <img src={gif} alt="" />
+
     </>
   )
 }
