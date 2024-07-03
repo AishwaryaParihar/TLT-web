1532e20bbfd6357e245680247f079189ebc7ae65
aditya <asadityasingh2572000@gmail.com>
2024-07-03 10:57:29 +0530

    fee card made by aditya

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './component/router/index.jsx'
-import Navbar from './component/Navbar.jsx'
-import Header from './component/Header.jsx'
+import React from "react";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./component/router/index.jsx";
+import Navbar from "./component/Navbar.jsx";
+import Header from "./component/Header.jsx";
 
 function App() {
-
-
   return (
     <>
-   <Header/>
-   <RouterProvider router={router}/>
+      <Header />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
