d91af67764b589cc8b79c3f047312484f2bc5ce6
karishma <karishmadawar123@gmail.com>
2024-07-03 13:21:57 +0530

    imp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import "./App.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./component/router/index.jsx";
-import Navbar from "./component/Navbar.jsx";
-import Header from "./component/Header.jsx";
-import Footer from "./component/Footer.jsx";
-=======
 import React from 'react'
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
@@ -14,20 +5,13 @@ import { router } from './component/router/index.jsx'
 import Navbar from './component/Navbar.jsx'
 import Header from './component/Header.jsx'
 import Footer from './component/Footer.jsx'
->>>>>>> d0cac02e3cc1abc973fbd460f71df394f59c9575
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Header />
-      <RouterProvider router={router} />
-      <Footer />
-=======
    <Header/>
    <RouterProvider router={router}/>
    <Footer/>
->>>>>>> d0cac02e3cc1abc973fbd460f71df394f59c9575
     </>
   );
 }
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-import React from "react";
-import Testimony from "./content/Testimony";
-import FeaturesCard from "./content/FeaturesCard";
-import Fee from "./content/Fee";
-
-export const Home = () => {
-  return (
-    <div>
-      <Testimony />
-      <Fee />
-      <FeaturesCard />
-    </div>
-  );
-};
-=======
 import React from 'react'
 import  Testimony  from './content/Testimony'
-import  FeaturesCard  from './content/FeaturesCard'
+// import  FeaturesCard  from './content/FeaturesCard'
 import Aboutus from './content/Aboutus'
 
 
@@ -29,4 +13,3 @@ export const Home = () => {
     </>
   )
 }
->>>>>>> d0cac02e3cc1abc973fbd460f71df394f59c9575
