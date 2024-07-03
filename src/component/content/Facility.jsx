e1f7ea262d284cb6ab7bd109ffed28e11a509e40
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-03 13:24:19 +0530

    imp

--- src/component/content/Facility.jsx
+++ src/component/content/Facility.jsx
@@ -1,32 +1,3 @@
-
-
-// import React from 'react';
-
-// const Facility = () => {
-//   return (
-//     <div className="p-4">
-//       <h1 className="text-center text-black text-2xl md:text-4xl mb-4">Amenities</h1>
-//       <div className="text-justify shadow p-8 m-20 text-lg">
-//        <ul>
-//        <ol> 1. Air Conditioned and ventilated classrooms. </ol><br></br>
-
-//         2. Teaching through interactive methods via Smartboard.<br></br>
-//         3. Supreme Court Live proceedings telecast on LED TVs.<br></br>
-//         4. Speakers installed for better audio in Classroom.<br></br>
-//         5. For Safety purposes, smoke detector, Fire alarms, and extinguisher installed.<br></br>
-//         6. Complete library with systems for recovery classes.<br></br>
-//         7. Biometric based entry and Exit system for enrolled students.<br></br>
-//         8. 12 Cameras for surveillance and security purposes.<br></br>
-//         9. Availability of Water Cooler for clean and cold water.
-//         </ul>
-//       </div>
-      
-//     </div>
-//   );
-// };
-
-// export default Facility;
-
 import React from 'react';
 
 const Facility = () => {
--- src/component/content/FeaturesCard.jsx
+++ src/component/content/FeaturesCard.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
 const FeaturesCard = () => {
-<<<<<<< HEAD
-  return <div className="mx-5 md:mx-20">my home</div>;
-};
-=======
   return (
     <div className='mx-5 md:mx-20'>
 
     </div>
   )
 }
->>>>>>> d0cac02e3cc1abc973fbd460f71df394f59c9575
 
 export default FeaturesCard;
