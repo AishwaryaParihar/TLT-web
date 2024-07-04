b22cc9f6c6b6e77e3dd8d653cd046ef424c967b9
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-04 13:29:45 +0530

    imp

--- src/component/whatshap/Whatshap.jsx
+++ src/component/whatshap/Whatshap.jsx
@@ -6,7 +6,7 @@ function Whatshap() {
   return (
     <div>
       {" "}
-      <a target="_blank" href="https://wa.me/+918602440712">
+      <a target="_blank" href="https://wa.me/+918602440718">
         <img src={Whatsapp} className="whatsapp-img" alt="" />{" "}
       </a>
     </div>
