61ae122e0de11caddf2bfbc45ebbd5b183407ab7
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-04 15:39:20 +0530

    imp

--- src/component/content/Facility-image.jsx
+++ src/component/content/Facility-image.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import fc from "../../assets/fc-img.webp";
 const FacilityeImage = () => {
   return (
-    <div className=" flex justify-center items-center pt-6 sm:p-12  px-8">
+    <div className="  justify-center items-center pt-6 sm:p-12  px-8">
     
       <img src={fc} alt="facality" className="w-full sm:h-[400px] mb-10 h-[250px]"></img>
     </div>
