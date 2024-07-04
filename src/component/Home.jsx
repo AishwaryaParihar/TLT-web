14f49f1a54aaffc631d83bdb15625a11cfdf4302
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-04 15:34:50 +0530

    imp

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -7,25 +7,15 @@ import Contactus from "./content/Contactus";
 import Facility from "./content/Facility";
 import Fee from "./content/Fee";
 import Bestfeaturs from "./content/Bestfeaturs";
-<<<<<<< HEAD
 import YoutubeGallery from "./content/YoutubeGallery";
 import Feature from "./content/Feature";
-=======
-import YoutubeGallery from "./content/YoutubeGallery"
-import EnrollButton from "./content/EnrollButton";
-import Hambergur from "./content/Hambergur";
->>>>>>> e8928ad9378a8def65aaf7acc18d76659c90019d
 
 export const Home = () => {
   return (
     <>
       <Bestfeaturs />
       <Testimony />
-<<<<<<< HEAD
       <Feature />
-=======
-      <EnrollButton/>
->>>>>>> e8928ad9378a8def65aaf7acc18d76659c90019d
       {/* <FeaturesCard/> */}
       {/* <Aboutus /> */}
      
