d69c2cc38f68ef9a1f1e27613420a1c58cfcf15e
karishma <karishmadawar123@gmail.com>
2024-07-04 15:06:10 +0530

    imp

--- src/component/content/Mentor.jsx
+++ src/component/content/Mentor.jsx
@@ -4,16 +4,16 @@ import { data } from "../data/mentor";
 
 const Mentor = () => {
   return (
-    <section className="mx-3 md:mx-20 mt-20">
-      <div className="flex flex-wrap gap-5">
-        <div className="flex flex-wrap flex-col justify-center">
+    <section className="mx-3 md:mx-20 md:mt-20 mt-10">
+      <div className="flex flex-wrap gap-5 justify-center items-center">
+        <div className="flex flex-wrap flex-col justify-center items-center">
           <div className="full-border">
             <Headings heading={"h6"}>
               {data.post}
             </Headings>
           </div>
-          <div className="w-[400px] mt-7 leading-loose z-10">
-            <div className="flex flex-col flex-wrap text-8xl font-medium">
+          <div className="md:w-[350px] w-full mt-7 leading-loose z-10">
+            <div className="flex flex-col gap-3 flex-wrap md:text-7xl text-7xl font-medium">
               <span className="text-primary font-bold">Shubham</span> <span>Upadhyay</span> <span>Sir</span>
             </div>
           </div>
@@ -21,7 +21,7 @@ const Mentor = () => {
         <div className="w-[400px] z-0">
           <img src={data.image} alt="" />
         </div>
-        <div className="w-[350px] mt-[200px] leading-loose text-justify">
+        <div className="md:w-[300px] w-full md:mt-[200px] mt-10 leading-loose text-justify">
           {data.text}
         </div>
       </div>
