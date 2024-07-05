891707288c067c7f9fc2d1d74462f80f1b8de99b
karishma <karishmadawar123@gmail.com>
2024-07-05 15:44:59 +0530

    imp

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import AboutFirm from "./content/AboutFirm";
 import Mentor from "./content/Mentor";
 import Breadcrumbs from "./utiliti/breadcrumbs/Breadcrumbs";
+import MissionVision from "./content/MissionVision";
 
 const About = () => {
   return (
     <div>
      <Breadcrumbs>
      <AboutFirm />
-     <Mentor /></Breadcrumbs>
+     <Mentor />
+     <MissionVision/>
+     </Breadcrumbs>
     </div>
   );
 };
