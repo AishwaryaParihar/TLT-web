51084b276a4efb215eaf16a62adb38bb010e1fa0
karishma <karishmadawar123@gmail.com>
2024-07-05 15:39:38 +0530

    imp

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutFirm from "./content/AboutFirm";
 import Mentor from "./content/Mentor";
+import MissionVision from "./content/MissionVision";
 
 const About = () => {
   return (
     <div>
       <AboutFirm />
       <Mentor />
+      <MissionVision/>
     </div>
   );
 };
--- src/component/data/mentor.js
+++ src/component/data/mentor.js
@@ -5,5 +5,6 @@ export const data = {
   name: "Shubham Upadhyay Sir",
   post: "Solo Faculty",
   image: mentorImg,
-  text: "“Lacinia nulla porttitor faucibus hendrerit at amet tincidunt id dis sit sed enim sit sed tortor odio mi sapien consequat aliquam volutpat diam eleifend risus at tempor volutpat massa disodio mi sapien consequat aliquam volutpat diam eleifend risus at tempor volutpat massa dis.”",
+  text1: "“I have been a Judiciary aspirant myself. And this makes me understand your struggles with bare acts, commentary books, and regarding recent and landmark judgements pretty well. But most of all firm belief in the thought that hard work can even beat talent with right and robust guidance made me come up with the idea of a body which becomes the torch bearer of law student – THE LAW TALES. ”",
+  text2:"The idea is to provide students a prep technique which is a balance of tradition and state of art E-learning mechanism. In my decade long teaching career my vision has been to empower the judiciary aspirants with complete conceptual clarity via method of fun learning. It is obvious that law, no matter how interesting they seem in the first instance intent to become boring. That is where I come in, your friendly neighborhood Shubham Sir. With all the knowledge of movies, sports and what not it is my passion to impart legal knowledge, which is not only is easy for retention but also ensure selection, ““क्योंकि समझेगा Aspirant, तभी Select होगा Aspirant."
 };
