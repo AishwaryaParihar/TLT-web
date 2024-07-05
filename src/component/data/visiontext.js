0142279764a93593bae9e925c999c41a7f6eb556
karishma <karishmadawar123@gmail.com>
2024-07-05 17:06:48 +0530

    imp

--- src/component/data/visiontext.js
+++ src/component/data/visiontext.js
@@ -1,39 +1,27 @@
-// export const data =
-//   {
-//     vision:
-//       "Our judicial academy’s vision is akin to a guiding star, illuminating the path towards excellence in the legal arena. It encompasses the overarching goal of shaping adept, ethical, and insightful legal minds poised to serve society with integrity and proficiency. Our judicial academy aspires not only to equip its students with legal knowledge but also to instill in them a deep sense of responsibility, empathy, and commitment to justice so as we.",
-
-//     mission:
-//       "The mission of our judicial academy transcends the mere transmission of legal concepts; it is about nurturing the seeds of legal brilliance within each student. Our mission-driven institute is dedicated to providing comprehensive guidance, personalized attention, and unwavering support to its students. It pledges to cultivate critical thinking, analytical prowess, and ethical integrity, essential qualities for success in the judicial realm.",
-
-//     mission2:
-//       "Moreover, a judiciously crafted mission entails fostering a culture of collaboration, where students engage in rigorous academic discourse, learn from one another, and collectively strive towards excellence. It underscores the importance of holistic development, encompassing not only legal acumen but also communication skills, emotional intelligence, and resilience in the face of challenges.",
-
-//     conclusion:
-//       "In the pursuit of judicial excellence, our academy will emerge as a beacon of guidance and support for aspiring legal luminaries. With a clear vision, a steadfast mission, and a well-defined pathway, our academy empowers students to realize their full potential, navigate the intricacies of the legal profession, and uphold the principles of justice with unwavering dedication. Aspiring judges and legal professionals who embark on this transformative journey emerge not only as proficient legal minds but also as custodians of justice, poised to make a meaningful impact on society.",
-//   }
-
+import visionImg from "../../assets/gif/shared-vision.png"
+import ideaImg from "../../assets/gif/lightbulb.png"
+import missionImg from "../../assets/gif/target.png"
 export const cardData = [
   {
     number: "01",
     title: "Vision",
     description:
       "Our judicial academy’s vision is akin to a guiding star, illuminating the path towards excellence in the legal arena. It encompasses the overarching goal of shaping adept, ethical, and insightful legal minds poised to serve society with integrity and proficiency. Our judicial academy aspires not only to equip its students with legal knowledge but also to instill in them a deep sense of responsibility, empathy, and commitment to justice so as we.",
-    image: "",
+    image: visionImg,
   },
   {
     number: "02",
     title: "Mission",
     description:
       "The mission of our judicial academy transcends the mere transmission of legal concepts; it is about nurturing the seeds of legal brilliance within each student. Our mission-driven institute is dedicated to providing comprehensive guidance, personalized attention, and unwavering support to its students. It pledges to cultivate critical thinking, analytical prowess, and ethical integrity, essential qualities for success in the judicial realm.",
-    image: "",
+    image: missionImg,
   },
   {
     number: "03",
     title: "Idea",
     description:
       "The idea is to provide students a prep technique which is a balance of tradition and state of art E-learning mechanism. In my decade long teaching career my vision has been to empower the judiciary aspirants with complete conceptual clarity via method of fun learning. It is obvious that law, no matter how interesting they seem in the first instance intent to become boring. That is where I come in, your friendly neighborhood Shubham Sir. With all the knowledge of movies, sports and what not it is my passion to impart legal knowledge, which is not only is easy for retention but also ensure selection, ““क्योंकि समझेगा Aspirant, तभी Select होगा Aspirant",
-    image: "",
+    image: ideaImg,
   },
   {
     number: "04",
