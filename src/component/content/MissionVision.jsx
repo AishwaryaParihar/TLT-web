f3064f84d1a296ff21940761a2d39481a8bf4197
karishma <karishmadawar123@gmail.com>
2024-07-06 12:34:16 +0530

    imp

--- src/component/content/MissionVision.jsx
+++ src/component/content/MissionVision.jsx
@@ -7,19 +7,20 @@ const MissionVision = () => {
     <div className="mx-3 md:mx-20 md:mt-20 mt-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {cardData.map((item, index) => (
-          <div className="rounded-lg bg-gray-50 overflow-hidden shadow-md group hover:border-red-500 hover:shadow-lg transition duration-300" key={index}>
-            <div className="bg-red-500 rounded-t-2xl overflow-hidden flex justify-evenly items-center p-3">
-            <Headings heading="h3" className="text-xl font-bold mb-2" style={"text-white text-shadow"}>
-                {item.title}
-              </Headings>
+          <div className="rounded-lg flex flex-shrink-0 bg-gray-50 overflow-hidden shadow-md group hover:border-red-500 hover:shadow-lg transition duration-300" key={index}>
+            <div className="bg-red-500 flex justify-center items-center p-5"> 
               <img
                 src={item.image}
                 alt={item.title}
-                className="w-[60px] h-[60px] object-cover"
+                className="object-cover"
+                
               />
               
             </div>
             <div className="p-5">
+            <Headings heading="h3" className="text-xl font-bold mb-2">
+                {item.title}
+              </Headings>
               <p className="">{item.description}</p>
             </div>
           </div>
