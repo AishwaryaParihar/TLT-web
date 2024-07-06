2951115409d26f3a69e0c537638f03452f679878
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-06 11:10:02 +0530

    imp

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,14 +5,8 @@ import Fee from "./content/Fee";
 import Bestfeaturs from "./content/Bestfeaturs";
 import YoutubeGallery from "./content/YoutubeGallery";
 import Feature from "./content/Feature";
-<<<<<<< HEAD
-import ReturnAndRefund from "./content/ReturnAndRefund";
-import EmpowermentBatch from "./content/Empowerment-batch"
-import RegistrationDetails from "./content/RegistrationDetails";
-=======
 import AboutUs from "./content/Aboutus";
 
->>>>>>> 0196ca1fda6c1d9619b5124de99ee519e2da3b94
 
 export const Home = () => {
   return (
@@ -23,13 +17,7 @@ export const Home = () => {
       <Fee />
       <YoutubeGallery />
       <Facility />
-<<<<<<< HEAD
-      <ReturnAndRefund/>
-      <EmpowermentBatch/>
-      <RegistrationDetails/>
-=======
       <Testimony />
->>>>>>> 0196ca1fda6c1d9619b5124de99ee519e2da3b94
     </>
   );
 };
