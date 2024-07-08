07d38626a01d6e853b472a01c79fdc8d6af8750d
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-07-08 13:34:36 +0530

    add product card

--- src/component/content/MpcjproductCard.jsx
+++ src/component/content/MpcjproductCard.jsx
@@ -1,81 +1,69 @@
-import React from 'react';
-import cardImage1 from '../../assets/productImage3.jpg';
-import cardImage2 from '../../assets/productImage4.jpg';
-import cardImage3 from '../../assets/productImage.jpg';
-import cardImage4 from '../../assets/productImage2.jpg';
+import React from "react";
+import Headings from "../utiliti/heading/Heading";
+import styless from "./MpcjproductCard.module.css";
+import atoz from "../../assets/dictionary.png";
+import tbmbasic from "../../assets/notes.png";
+import tbmplus from "../../assets/copy-writing.png";
+import tbmad from "../../assets/creative-writing.png";
 
-const products = [
+const freedata = [
   {
-    id: 1,
-    name: 'MPCJ Paper-2 & CGCJ',
-    href: '/MockTestCard',
-    imageSrc: cardImage1,
-    imageAlt: "Front of men's Basic Tee in black.",
-    subName: 'Translator Material',
-    price: 'Know More',
+    img: atoz,
+    title: "A-Z LEGAL GLOSSARY",
+    price: "Rs-399/-",
+    description: "for Legal glossary (1500+ Words) (HANDPICKED)",
   },
   {
-    id: 2,
-    name: 'MPCJ offline mock test series',
-    href: '/MpcjproductCard',
-    imageSrc: cardImage2,
-    imageAlt: "Front of men's Basic Tee in black.",
-    subName: 'Test Material',
-    price: 'Know More',
+    img: tbmbasic,
+    title: "TPM BASIC",
+    price: "Rs-699/-",
+    description:
+      "the TPM Basic Pack(Total 200 one liners Criminal- 100 one liners Civil- 100 one liners)",
   },
   {
-    id: 3,
-    name: 'MPCJ Paper-1 Study Material',
-    href: '/StudyMaterialCard',
-    imageSrc: cardImage3,
-    imageAlt: "Front of men's Basic Tee in black.",
-    subName: 'Study Material',
-    price: 'Know More',
+    img: tbmplus,
+    title: "TPM PLUS",
+    price: "Rs-999/-",
+    description:
+      " for the TPM Plus Pack (Total 400 one liners Criminal- 200 one liners Civil- 200 one liners)",
   },
   {
-    id: 4,
-    name: 'MPCJ Online Test Series',
-    href: '/OnlineTestCard',
-    imageSrc: cardImage4,
-    imageAlt: "Front of men's Basic Tee in black.",
-    subName: 'Test Series',
-    price: 'Know More',
+    img: tbmad,
+    title: "TPM ADVANCED",
+    price: "Rs-1299/-",
+    description:
+      " for the TPM Advanced (Total 400 one liners Criminal- 200 one liners Civil- 200 one liners with Legal glossary of 1500+ words)",
   },
 ];
 
 const MpcjproductCard = () => {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our Products</h2>
+    <>
+      <div className="flex flex-col justify-items-center items-center p-5 md:p-20 ">
+        <div className="flex flex-col justify-items-start self-start md:ms-16">
+          <Headings heading={"h2"} style={"text-shadow"}> <span className="text-primary">MPCJ</span> and <span className="text-primary">CGCJ</span> 2024 (Mains)</Headings>
 
-        <div className="mt-6  grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden shadow-xl rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0"></span>
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.subName}</p>
+          <Headings heading={"h3"}>Exclusive Practice Material</Headings>
+
+          <Headings heading={"h5"}>Starting From 2nd February 2024</Headings>
+        </div>
+
+        <div className="flex justify-center items-center flex-wrap">
+          {freedata.map((items) => (
+            <div className={styless.card_style}>
+              <div className={styless.content}>
+                <div className={styless.icons_div}>
+                  <img src={items.img} className={styless.icons} />
                 </div>
-                <p className="text-sm rounded-lg pt-3 px-2 font-semibold bg-red-500 text-white">{product.price}</p>
+                <div className={styless.title}>{items.title}</div>
+                <div className={styless.price}>{items.price}</div>
+                <div className={styless.description}>{items.description}</div>
               </div>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
