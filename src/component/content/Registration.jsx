469e9e824e82c42f50cca5ced0d5326982a3e534
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-08 16:02:02 +0530

    imp

--- src/component/content/Registration.jsx
+++ src/component/content/Registration.jsx
@@ -1,16 +1,45 @@
 
+// import React from 'react';
+// import styles from './registration.module.css';
+
+// const Registration = () => {
+//   return (
+//     <div className="">
+//       <div className='flex flex-col justify-center items-center text-center mt-10'>
+//         <h1 className='font-bold text-5xl'>TLT <span className='text-primary'>JUDICIAL</span> ACADEMY</h1>
+//         <h3 className='text-black text-1xl font-bold sm:font-semibold sm:text-3xl mt-3'>2ND FLOOR, JYOTI CINEPLEX, MP NAGAR</h3>
+//         <h3 className='text-black text-1xl font-bold sm:font-semibold sm:text-3xl mt-3'>ZONE 1, BHOPAL, MOB: 9238176156</h3>
+//         <h1  className={styles.formBorder} >REGISTRATION FORM</h1>
+//         <h3  className='text-black text-1xl font-bold sm:font-semibold  sm:text-3xl mt-6'>2024-2025 Session</h3>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Registration;
+
 import React from 'react';
 import styles from './registration.module.css';
 
 const Registration = () => {
   return (
-    <div className="">
+    <div className="px-4 sm:px-6 lg:px-8">
       <div className='flex flex-col justify-center items-center text-center mt-10'>
-        <h1 className='font-bold text-5xl'>TLT <span className='text-primary'>Judicial</span> Academy</h1>
-        <h3 className='text-black text-1xl font-bold sm:font-semibold sm:text-3xl mt-3'>2ND FLOOR, JYOTI CINEPLEX, MP NAGAR</h3>
-        <h3 className='text-black text-1xl font-bold sm:font-semibold sm:text-3xl mt-3'>ZONE 1, BHOPAL, MOB: 9238176156</h3>
-        <h1  className={styles.formBorder} >REGISTRATION FORM</h1>
-        <h3  className='text-black text-1xl font-bold sm:font-semibold  sm:text-3xl mt-6'>2024-2025 Session</h3>
+        <h1 className='font-bold text-3xl sm:text-4xl lg:text-5xl'>
+          TLT <span className='text-primary'>JUDICIAL</span> ACADEMY
+        </h1>
+        <h3 className='text-black text-sm sm:text-lg lg:text-xl font-bold sm:font-semibold mt-3'>
+          2ND FLOOR, JYOTI CINEPLEX, MP NAGAR
+        </h3>
+        <h3 className='text-black text-sm sm:text-lg lg:text-xl font-bold sm:font-semibold mt-3'>
+          ZONE 1, BHOPAL, MOB: 9238176156
+        </h3>
+        <h1 className={`${styles.formBorder} text-2xl sm:text-3xl lg:text-4xl mt-6`}>
+          REGISTRATION FORM
+        </h1>
+        <h3 className='text-black text-sm sm:text-lg lg:text-xl font-bold sm:font-semibold mt-6'>
+          2024-2025 Session
+        </h3>
       </div>
     </div>
   );
