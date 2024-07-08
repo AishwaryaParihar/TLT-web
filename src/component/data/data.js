8d0293e0e85ed4f974b6bfda667d88bccc93fbd2
aditya <asadityasingh2572000@gmail.com>
2024-07-08 10:35:21 +0530

    add Jet multi setps form

---
+++ src/component/data/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    step: "step 1",
+    title: "your info",
+    linkTo: "/",
+  },
+  {
+    id: 2,
+    step: "step 2",
+    title: "Guardian Details",
+    linkTo: "Guardiandetails",
+  },
+  {
+    id: 3,
+    step: "step 3",
+    title: "Documents Upload",
+    linkTo: "Documents",
+  },
+  {
+    id: 4,
+    step: "step 4",
+    title: "Educational",
+    linkTo: "Educational",
+  },
+  {
+    id: 5,
+    step: "step 5",
+    title: "Conset",
+    linkTo: "Conset",
+  },
+];
+
+
