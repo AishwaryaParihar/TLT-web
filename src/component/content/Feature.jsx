be934050ad20be853beb392503de568e247e2e66
aditya <asadityasingh2572000@gmail.com>
2024-07-09 11:54:31 +0530

    remove cards from feature section and also add soft skill card , content and image

--- src/component/content/Feature.jsx
+++ src/component/content/Feature.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Headings from "../utiliti/heading/Heading";
 import stylees from "./Feature.module.css";
-import result from "../../assets/result3.jpg";
 import studentf from "../../assets/students.jpg";
-import doubt from "../../assets/doubt.jpg";
-import OneOn from "../../assets/oneon.jpg";
+import Soft from "../../assets/softskill.webp";
 import solo from "../../assets/solo.jpg";
 import learning from "../../assets/interactive.jpg";
 import study from "../../assets/studey.jpeg";
@@ -23,30 +21,14 @@ const facalitydata = [
     fdescription:
       "We offer engaging sessions to enhance understanding of complex legal concepts, staying at the forefront of educational trends with innovative learning methods.Our curriculum includes interactive sessions & online resources, providing students with a dynamic learning environment.",
   },
-  {
-    fimg: OneOn,
-    fname: "One-on-One Mentoring",
-    fdescription:
-      "Each student at TLT Judicial Academy receives personalized attention through one-on-one mentoring sessions. This individualized approach ensures that every student's unique needs and challenges are addressed effectively.",
-  },
-  {
-    fimg: result,
-    fname: "Result Centric Preparation",
-    fdescription:
-      "We prioritize practical success by focusing on strategies and techniques that lead to favorable outcomes in judiciary examinations. Our tailored approach ensures that students are well-prepared to achieve their desired results.",
-  },
+
   {
     fimg: studentf,
     fname: "Student Friendly Ecosystem",
     fdescription:
       "Our academy fosters a supportive & inclusive environment where students feel comfortable & motivated to excel. We prioritize student satisfaction & strive to create a nurturing ecosystem conducive to learning & growth.",
   },
-  {
-    fimg: doubt,
-    fname: "Doubt Clearing Sessions ",
-    fdescription:
-      "We understand the importance of addressing students' doubts promptly.That's why we offer dedicated doubt-clearing sessions with swift resolution, ensuring that no question goes unanswered & students can progress with confidence.",
-  },
+
   {
     fimg: study,
     fname: "Updated & Hand-Picked Study Material",
@@ -55,9 +37,15 @@ const facalitydata = [
   },
   {
     fimg: mock,
-    fname: "Mock Tests and Performance Analysis",
+    fname: "Mock Tests & Performance Analysis",
     fdescription:
       "Practice is key to success in judiciary exams. At TLT Judicial Academy, we emphasize regular mock tests and performance evaluations so students can assess progress, identify improvement areas, and refine their preparation.",
+  },
+  {
+    fimg: Soft,
+    fname: "Soft Skill Development",
+    fdescription:
+      "where students engage in rigorous academic discourse, learn from one another, & collectively strive towards excellence.It underscores the importance of holistic development, encompassing not only legal acumen but also communication skills, emotional intelligence, and resilience in the face of challenges.",
   },
 ];
 const Feature = () => {
@@ -65,24 +53,27 @@ const Feature = () => {
     <>
       <div className="mt-16">
         <div className="py-5 px-5 md:px-20">
-        <Headings heading={'h6'} style={'text-shadow'}>Let's make your dream into reality</Headings>
-        <Headings heading={'h2'} style={'text-shadow'}>Our Best <span className="text-primary">
-        Features</span></Headings>
+          <Headings heading={"h6"} style={"text-shadow"}>
+            Let's make your dream into reality
+          </Headings>
+          <Headings heading={"h2"} style={"text-shadow"}>
+            Our Best <span className="text-primary">Features</span>
+          </Headings>
         </div>
-      <div className="flex justify-center items-center  flex-wrap gap-5 pb-16 ">
-        {facalitydata.map((items, index) => (
-          <div key={index} className={stylees.card}>
-            <img className={stylees.imgbox} src={items.fimg} />
-            <div className={stylees.content}>
-              <div className={stylees.description}>
-                <Headings heading={"h5"}>{items.fname}</Headings>
-              </div>
+        <div className="flex justify-center items-center  flex-wrap gap-14 pb-16 ">
+          {facalitydata.map((items, index) => (
+            <div key={index} className={stylees.card}>
+              <img className={stylees.imgbox} src={items.fimg} />
+              <div className={stylees.content}>
+                <div className={stylees.description}>
+                  <Headings heading={"h5"}>{items.fname}</Headings>
+                </div>
 
-              <div className={stylees.info}>{items.fdescription}</div>
+                <div className={stylees.info}>{items.fdescription}</div>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </>
   );
