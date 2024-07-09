2ab70bf1b2149ac3f074174f78c247b9c85c1625
karishma <karishmadawar123@gmail.com>
2024-07-09 13:15:04 +0530

    imp

--- src/component/content/MissionVision.jsx
+++ src/component/content/MissionVision.jsx
@@ -5,9 +5,9 @@ import Headings from "../utiliti/heading/Heading";
 const MissionVision = () => {
   return (
     <div className="mx-3 md:mx-20 md:mt-20 mt-10">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6">
         {cardData.map((item, index) => (
-          <div className="rounded-lg flex bg-gray-50 overflow-hidden shadow-md group hover:border-red-500 hover:shadow-lg transition duration-300" key={index}>
+          <div className="rounded-lg flex bg-gray-50 overflow-hidden shadow-lg group hover:border-red-500 hover:shadow-lg transition duration-300" key={index}>
             <div className="bg-red-500 flex justify-center w-[500px] items-center p-3"> 
               <img
                 src={item.image}
@@ -15,11 +15,11 @@ const MissionVision = () => {
                 className=""
               /> 
             </div>
-            <div className="p-3">
+            <div className=" p-5">
             <Headings heading="h3" className="text-xl font-bold">
                 {item.title}
               </Headings>
-              <p className="text-sm">{item.description}</p>
+              <p className="">{item.description}</p>
             </div>
           </div>
         ))}
