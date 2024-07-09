20cb616d4240636045a9cc1d6aeb127661d80430
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-09 12:57:19 +0530

    imp

--- src/component/content/Facility-image.jsx
+++ src/component/content/Facility-image.jsx
@@ -33,21 +33,21 @@ import facility from "../../assets/facility.webp";
 
 const FacilityeImage = () => {
   return (
-    <div className="flex flex-wrap justify-evenly gap-10 items-center relative">
-      <div className="flex flex-wrap justify-center gap-4 ml-8 ms:ml-8">
-        <div className="flex flex-col px- gap-3 md:gap-4 mt-[50px] md:mt-0 lg:mt-[0px]">
+    <div className="flex flex-wrap justify-evenly gap-2 items-center relative">
+      <div className="flex flex-wrap sm:flex-nowrap justify-center gap-2 sm:mt-10 ml-2 ms:ml-8">
+        <div className="flex flex-col px- gap-3 md:gap-2 mt-[50px] md:mt-0 lg:mt-[0px]">
 
-          <div className="w-[150px] h-[120px] sm:w-[150px] sm:h-[150px] md:w-[300px] md:h-[220px]">
+          <div className="w-[140px] h-[110px] sm:w-[150px] sm:h-[150px] md:w-[300px] md:h-[220px]">
             <img src={facility} alt="pen image" className="w-full h-full object-cover rounded-lg" />
           </div>
 
-          <div className="w-[150px] h-[120px] sm:w-[150px] sm:h-[150px] md:w-[300px] md:h-[220px]">
+          <div className="w-[140px] h-[110px] sm:w-[150px] sm:h-[150px] md:w-[300px] md:h-[220px]">
             <img src={fc} alt="hammer image" className="w-full h-full object-cover rounded-lg" />
           </div>
         </div>
 
-        <div className="mt-12 px-4 my-0 md:mt-0 lg:mt-0 ">
-          <img src={court} alt="justice image" className="w-[150px] h-[260px] sm:w-[200px] sm:h-[300px] md:w-[300px] md:h-[450px] object-cover rounded-lg" />
+        <div className="mt-12 px-0 my-0 md:mt-0 lg:mt-0 ">
+          <img src={court} alt="justice image" className="w-[140px] h-[250px] sm:w-[200px] sm:h-[300px] md:w-[300px] md:h-[450px] object-cover rounded-lg" />
         </div>
       </div>
     </div>
--- src/component/content/ListItemHoverEffect.jsx
+++ src/component/content/ListItemHoverEffect.jsx
@@ -1,49 +1,12 @@
-// import React from "react";
 
-// const ListItemHoverEffect = () => {
-//   return (
-//     <div className="items-center px-18 sm:mx-20">
-//       <div className="relative">
-//         <h2 className="text-white font-bold tracking-wide mb-2">
-//           List Item Hover Effect
-//         </h2>
-//         <ul className="relative mb-8">
-//           {[
-//             "Air Conditioned and ventilated classrooms.",
-//             "Teaching through interactive methods via Smartboard.",
-//             "Supreme Court Live proceedings telecast on LED TVs.",
-//             "Speakers installed for better audio in Classroom.",
-//             "For Safety purposes, smoke detector, Fire alarms and extinguisher installed.",
-//             "Complete library with systems for recovery classes.",
-//             "Biometric based entry and Exit system for enrolled students",
-//             "12 Cameras for surveillance and security purposes.",
-//             "vailability of Water Cooler for clean and cold water.",
-//           ].map((text, index) => (
-//             <li
-//               key={index}
-//               className="relative  list-none my-1 pl-4 border-l-2 border-red-500 transition-transform duration-500 cursor-pointer hover:translate-x-2 group"
-//             >
-//               <span className="relative inline-block py-2 pl-3 transition-colors duration-500 group-hover:text-white group-hover:font-semibold z-10">
-//                 {text}
-//               </span>
-//               <div className="absolute inset-0 bg-primary transform scale-x-0 origin-left transition-transform duration-500 group-hover:scale-x-100"></div>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ListItemHoverEffect;
 import React from "react";
 
 const ListItemHoverEffect = () => {
   return (
 
-    <div className="flex flex-col items-center px-4 sm:px-8 lg:px-18 sm:mx-4 lg:mx-20">
+    <div className="flex flex-wrap sm:flex-wrap items-center px-4 sm:px-8 lg:px-18 sm:mx-4 lg:mx-20">
    
-      <div className="relative w-full max-w-4xl">
+      <div className=" ">
      
         <ul className="relative mb-8 w-full">
           {[
