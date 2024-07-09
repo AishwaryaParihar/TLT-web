1e3b1f8c953aaac40e050adad87e3cecee95e614
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-09 13:26:31 +0530

    imp

--- src/component/content/ReturnAndRefund.jsx
+++ src/component/content/ReturnAndRefund.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const ReturnAndRefund = () => {
   return (
-    <div className="px-5 md:px-20 ">
-    <div className="sm:m-20 sm:p-0 p-4 bg-white  rounded-lg">
-      <h2 className="text-center font-bold text-4xl text-black mb-4 ">Refund <span className='text-primary'>Policy</span></h2>
-      <p className="pt-2 text-lg leading-relaxed">
+    <div className="px-5 md:px-20 mb-10 ">
+    <div className=" bg-white  rounded-lg">
+      <h2 className="text-center font-bold text-4xl text-black mt-5 mb-3 ">Refund <span className='text-primary'>Policy</span></h2>
+      <p className="pt-1 text-md leading-relaxed">
         We at The Law Tales always aim at identifying the issue and helping you fix it. However, if the issue still persists, email us your concerns & course details from your registered email ID to <a href="mailto:support@thelawtales.in" className="text-blue-500 underline">support@thelawtales.in</a>. Our team will connect with you.
       </p>
       
-      <h1 className="font-bold text-2xl sm:pt-8 pt-4 ">Our refund policy is:</h1>
-      <ul className="text-lg list-disc list-inside mt-4 space-y-4">
+      <h1 className="font-bold text-2xl sm:pt-2 pt-1 ">Our refund policy is:</h1>
+      <ul className="text-md list-disc list-inside mt-4 space-y-4">
         <li>
           Refunds are only allowed within 3 days of enrollment. A 100% refund (after deducting GST and payment gateway charges) will be paid to the candidate. Refund requests will not be processed after 3 days of enrollment. No refund requests shall be made with respect to the Tests & Course modules.
         </li>
@@ -25,9 +25,9 @@ const ReturnAndRefund = () => {
         </li>
       </ul>
       
-      <h2 className="text-center font-bold text-4xl text-primary sm:pt-10 pt-8 ">Return <span className='text-black'>Policy</span></h2>
+      <h2 className="text-center font-bold text-4xl text-primary sm:pt-4 pt-8 ">Return <span className='text-black'>Policy</span></h2>
       <h1 className="font-bold text-2xl sm:pt-6 pt-6 ">No refund after Delivery of the product:</h1>
-      <p className="sm:pt-5 pt-4 text-lg leading-relaxed">
+      <p className="sm:pt-1 pt-4 text-md leading-relaxed">
         The delivery of our product will be made within 2 days of the payment. The delivery will be done through online mode via Whatsapp/Telegram in PDF format hence no refund will be made once delivery.
       </p>
     </div>
