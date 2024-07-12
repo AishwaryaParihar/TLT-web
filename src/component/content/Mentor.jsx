09287e817f25c59d41e6620f0e2f07df1013d41b
karishma <karishmadawar123@gmail.com>
2024-07-12 14:19:52 +0530

    imp

--- src/component/content/Mentor.jsx
+++ src/component/content/Mentor.jsx
@@ -4,24 +4,26 @@ import { data } from "../data/mentor";
 
 const Mentor = () => {
   return (
-    <section className="mx-3 md:mx-20 md:my-20 mt-10">
-      <div className="flex flex-wrap gap-5 justify-center items-center">
+    <section className="mx-5 md:mx-16 lg:mx-20 md:my-20 mt-10">
+      <div className="flex flex-wrap justify-center items-center">
+        <div className="md:gap-5 lg:gap-0 sm:gap-20 aboutMentor">
         <div className="flex flex-wrap flex-col justify-center items-center">
           <div className="full-border">
             <Headings heading={"h6"}>
               {data.post}
             </Headings>
           </div>
-          <div className="md:w-[320px] w-full mt-7 leading-loose z-10">
+          <div className="lg:w-[320px] w-full mt-7 leading-loose z-10">
             <div className="flex flex-col gap-3 flex-wrap md:text-7xl text-7xl font-medium">
               <span className="text-primary font-bold">Shubham</span> <span>Upadhyay</span> <span>Sir</span>
             </div>
           </div>
         </div>
-        <div className="w-[350px] z-0">
+        <div className="w-[350px] lg:w-[350px] z-0">
           <img src={data.image} alt="" />
         </div>
-        <div className="w-[400px] md:mt-16 mt-10 text-justify">
+        </div>
+        <div className="lg:w-[400px] w-full md:mt-16 mt-10 text-justify">
           {data.text1}
         </div>
       </div>
